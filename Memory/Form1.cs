b4c1e375c173cd6155a824976718a851ae4b849a
Piotr Kościuk <piotr.kosciuk87@gmail.com>
2019-09-24 16:17:08 +0200

    add few methods and general game layout

--- Memory/Form1.cs
+++ Memory/Form1.cs
@@ -12,9 +12,26 @@ namespace Memory
 {
     public partial class Form1 : Form
     {
+        private bool[] doesButtonHaveAColor = new bool[17];
+        private Color[] colorsList = new Color[17];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void randomButton(Color color) // losuj przyciski i przypisz mu kolor, jeśli przycisk nie jest zajęty;
+        {
+            Random generator = new Random();
+            int fieldNumber = generator.Next(1, 17); // losuj numer pola;
+
+            while (doesButtonHaveAColor[fieldNumber] == true) // jeśli wylosowany numer pola już został wcześniej wylosowany losuj dalej, jeśli nie wyjdź z pętli;
+            {
+                fieldNumber = generator.Next(1, 17);
+            }
+
+            colorsList[fieldNumber] = color;
+            doesButtonHaveAColor[fieldNumber] = true;
         }
     }
 }
